3f91e434712d0ee3cd704a1d46120fde77cfd2dd
Phillip Chang <lasagnaphil@snu.ac.kr>
2020-12-18 00:55:10 +0900

    Port for windows

--- demo/contacts.cpp
+++ demo/contacts.cpp
@@ -32,9 +32,9 @@ int main(void)
 
     // Define the camera to look into our 3d world
     Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 5.0f, 5.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.position = Vector3 { 0.0f, 5.0f, 5.0f };
+    camera.target = Vector3 { 0.0f, 0.0f, 0.0f };
+    camera.up = Vector3 { 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
     camera.type = CAMERA_PERSPECTIVE;
     SetCameraMode(camera, CAMERA_THIRD_PERSON);
