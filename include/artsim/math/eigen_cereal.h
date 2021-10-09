afadfd990ce666456fe98abc15d0b0244e041d8f
Phillip Chang <phil@mrl.snu.ac.kr>
2021-10-09 23:32:40 +0900

    update eigen

--- include/artsim/math/eigen_cereal.h
+++ include/artsim/math/eigen_cereal.h
@@ -12,34 +12,49 @@
 namespace cereal {
 template <class Archive, class Derived>
 inline
-typename std::enable_if<traits::is_output_serializable<BinaryData<typename Derived::Scalar>, Archive>::value, void>::type
+void // typename std::enable_if<cereal::traits::is_output_serializable<cereal::BinaryData<typename Derived::Scalar>, Archive>::value, void>::type
 save(Archive& ar, Eigen::PlainObjectBase<Derived> const& m) {
     typedef Eigen::PlainObjectBase<Derived> ArrT;
     if (ArrT::RowsAtCompileTime == Eigen::Dynamic) ar(m.rows());
     if (ArrT::ColsAtCompileTime == Eigen::Dynamic) ar(m.cols());
-    ar(binary_data(m.data(), m.size() * sizeof(typename Derived::Scalar)));
+    ar(cereal::binary_data(m.data(), m.size() * sizeof(typename Derived::Scalar)));
 }
 
 template <class Archive, class Derived>
 inline
-typename std::enable_if<traits::is_input_serializable<BinaryData<typename Derived::Scalar>, Archive>::value, void>::type
+void // typename std::enable_if<cereal::traits::is_input_serializable<cereal::BinaryData<typename Derived::Scalar>, Archive>::value, void>::type
 load(Archive& ar, Eigen::PlainObjectBase<Derived>& m) {
     typedef Eigen::PlainObjectBase<Derived> ArrT;
     Eigen::Index rows = ArrT::RowsAtCompileTime, cols = ArrT::ColsAtCompileTime;
     if (rows == Eigen::Dynamic) ar(rows);
     if (cols == Eigen::Dynamic) ar(cols);
     m.resize(rows, cols);
-    ar(binary_data(m.data(), static_cast<std::size_t>(rows * cols * sizeof(typename Derived::Scalar))));
+    ar(cereal::binary_data(m.data(), static_cast<std::size_t>(rows * cols * sizeof(typename Derived::Scalar))));
 }
 
+/*
+template <class Archive, class Scalar, Eigen::Index Size>
+inline void save(Archive& ar, const Eigen::Vector<Scalar, Size>& v) {
+    if (Size == Eigen::Dynamic) ar(v.size());
+    ar(binary_data(v.data(), v.size() * sizeof(Scalar)));
+}
+
+template <class Archive, class Scalar, Eigen::Index Size>
+inline void load(Archive& ar, Eigen::Vector<Scalar, Size>& v) {
+    if (Size == Eigen::Dynamic) {
+        Eigen::Index size;
+        ar(size);
+        v.resize(size);
+    }
+    ar(binary_data(v.data(), v.size() * sizeof(Scalar)));
+}
+ */
+
 // From: https://stackoverflow.com/questions/24593085/serializing-decomposed-matrix-from-eigen-sparselu-object
-template <class Archive, class Derived>
-inline
-typename std::enable_if<traits::is_input_serializable<BinaryData<typename Derived::Scalar>, Archive>::value, void>::type
-load(Archive& ar, Eigen::SparseCompressedBase<Derived>& m) {
-    typedef Eigen::SparseCompressedBase<Derived> MatT;
-    typedef typename MatT::StorageIndex Index;
-    typedef typename MatT::Scalar Scalar;
+
+template <class Archive, class Scalar>
+inline void save(Archive& ar, const Eigen::SparseMatrix<Scalar>& m) {
+    typedef typename Eigen::SparseMatrix<Scalar>::StorageIndex Index;
     Index rows, cols, nnzs, outS, innS;
     rows = m.rows();
     cols = m.cols();
@@ -47,9 +62,40 @@ load(Archive& ar, Eigen::SparseCompressedBase<Derived>& m) {
     outS = m.outerSize();
     innS = m.innerSize();
     ar(rows, cols, nnzs, outS, innS);
-    ar(binary_data(m.valuePtr(), sizeof(Scalar) * nnzs));
-    ar(binary_data(m.outerIndexPtr(), sizeof(Index) * outS));
-    ar(binary_data(m.innerIndexPtr(), sizeof(Index) * innS));
+    ar(cereal::binary_data(m.valuePtr(), sizeof(Scalar) * nnzs));
+    ar(cereal::binary_data(m.outerIndexPtr(), sizeof(Index) * outS));
+    ar(cereal::binary_data(m.innerIndexPtr(), sizeof(Index) * innS));
+}
+
+template <class Archive, class Scalar>
+inline void load(Archive& ar, Eigen::SparseMatrix<Scalar>& m) {
+    typedef typename Eigen::SparseMatrix<Scalar>::StorageIndex Index;
+    Index rows, cols, nnzs, outS, innS;
+    ar(rows, cols, nnzs, outS, innS);
+    m.resize(rows, cols);
+    m.makeCompressed();
+    m.resizeNonZeros(nnzs);
+    ar(cereal::binary_data(m.valuePtr(), sizeof(Scalar) * nnzs));
+    ar(cereal::binary_data(m.outerIndexPtr(), sizeof(Index) * outS));
+    ar(cereal::binary_data(m.innerIndexPtr(), sizeof(Index) * innS));
+}
+
+template <class Archive, class Derived>
+inline void save(Archive& ar, const Eigen::SimplicialCholeskyBase<Derived>& ldlt) {
+    ar(ldlt.getInternalMatrix());
+    ar(ldlt.getInternalParent());
+    ar(ldlt.getInternalNonZerosPerCol());
+    ar(ldlt.permutationP().indices());
+    ar(ldlt.permutationPinv().indices());
+}
+
+template <class Archive, class Derived>
+inline void load(Archive& ar, Eigen::SimplicialCholeskyBase<Derived>& ldlt) {
+    ar(ldlt.getInternalMatrix());
+    ar(ldlt.getInternalParent());
+    ar(ldlt.getInternalNonZerosPerCol());
+    ar(ldlt.permutationP().indices());
+    ar(ldlt.permutationPinv().indices());
 }
 
 }
